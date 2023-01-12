2fa803ac07b85414a5ccddddf6626e205de002ec
jpdamon <jpdamon@gmail.com>
2023-01-11 20:55:01 -0500

    started on project, realized need better html/css foundation. Going back to foundation path

---
+++ javascript_course/2_project_library.js
@@ -0,0 +1,21 @@
+let myLibrary = []
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${
+      read ? "read" : "not read yet"
+    }`;
+  };
+}
+
+const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+myLibrary.push(hobbit)
+
+function addBookToLibrary() {
+
+}
+
